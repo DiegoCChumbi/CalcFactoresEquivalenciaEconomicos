7b18e6fab0a3b77b18558d2c72236f91e4badd4d
DiegoCChumbi <122504015+DiegoCChumbi@users.noreply.github.com>
2025-01-20 23:52:15 -0500

    Se ha agregado F/G

--- equivalencias.js
+++ equivalencias.js
@@ -15,21 +15,21 @@ document.getElementById("input").addEventListener('submit', function(event) {
     const equivalencia6 = i / ((1 + i) ** n - 1); // (A/F,i,n)
     const equivalencia7 = ((1+i)**n - (i*n) - 1)/((i**2) * (1+i)**n); // (P/G,i,n)
     const equivalencia8 = (1/i) - (n/(((1+i)**n) - 1)); // (A/G,i,n)
+    const equivalencia9 = ((1+i)**n-i*n-1)/(i**2); // (F/G,i,n)
 
-    let equivalencia9;
+    let equivalencia10;
     if (g === 0) {
-        equivalencia9 = "No aplicable";
+        equivalencia10 = "No aplicable";
     }else{
         if(g === i){
-            equivalencia9 = (n/(1+i)).toFixed(8);
+            equivalencia10 = (n/(1+i)).toFixed(8);
             console.log("Estoy usando la formula reducida");
             
         }else{
-            equivalencia9 = (((1-((1+g)/(1+i))**n))/(i-g)).toFixed(8);
+            equivalencia10 = (((1-((1+g)/(1+i))**n))/(i-g)).toFixed(8);
             console.log("Estoy usando la formula completa");
         }
     }
-
 
     document.getElementById('equivalencia1').value = equivalencia1.toFixed(8);
     document.getElementById('equivalencia2').value = equivalencia2.toFixed(8);
@@ -39,5 +39,6 @@ document.getElementById("input").addEventListener('submit', function(event) {
     document.getElementById('equivalencia6').value = equivalencia6.toFixed(8);
     document.getElementById('equivalencia7').value = equivalencia7.toFixed(8);
     document.getElementById('equivalencia8').value = equivalencia8.toFixed(8);
-    document.getElementById('equivalencia9').value = equivalencia9;
+    document.getElementById('equivalencia9').value = equivalencia9.toFixed(8);
+    document.getElementById('equivalencia10').value = equivalencia10;
 });
